58b4cf1fad4d8ffd67bcc7ac0a1350e479d435a4
Michael <>
2021-06-15 00:45:42 -0600

    tooltip data, font family

--- proyecto-d3/bundle.js
+++ proyecto-d3/bundle.js
@@ -19,29 +19,41 @@
         let temperatureData1 = temperatureData.filter(r => r.dt === date);
 
 
-        var formatDate = d3.timeFormat("%Y-%m-01");
+        d3.timeFormat("%Y-%m-01");
 
 
-        let previousYear = (new Date(date));
-        previousYear.setFullYear(previousYear.getFullYear() - 1);
-        previousYear.setMonth(previousYear.getMonth() + 1);
+        let getPreviousYear = (date, nYear) => {
+            let previousYear = (new Date(date));
+            previousYear.setFullYear(previousYear.getFullYear() - nYear);
+            let newDate = previousYear.getFullYear() + date.substring(4, 7);
 
-        console.log(date + '<------------');
-        console.log(formatDate(previousYear));
+            return newDate
+        };
 
-        // console.log(temperatureData.find(x => x.Country === countryName && x.dt === previousYear))
+        let getPreviousYearTemp = (date, nYear, country) => {
+            let previousYear = (new Date(date));
+            previousYear.setFullYear(previousYear.getFullYear() - nYear);
+            let newDate = previousYear.getFullYear() + date.substring(4, 10);
 
+            // console.log(`current date: ${date}`)
+            // console.log(`previous date: ${newDate}`)
+            // console.log(temperatureData)
 
+            let previousYearResult = temperatureData.find(r => r.dt === newDate && r.Country === country);
 
+            // console.log(previousYearResult)
 
+            if (typeof(previousYearResult) === 'undefined') {
+                return "No Data"
+            } else {
+                return (+previousYearResult.AverageTemperature).toFixed(2)
+            }
 
+        };
 
-
-
-
-
-
-
+        let calcTempDiff = (currentTemp, previousTemp) => {
+            return (currentTemp - previousTemp).toFixed(2)
+        };
 
 
         let countryTemp = countryName => {
@@ -56,10 +68,69 @@
 
         };
 
+        let generateRowTemplate = (date, countryName, currentTemp, yearRange) => {
+
+            let previousYear = getPreviousYear(date, yearRange);
+            let previousYearTemp = getPreviousYearTemp(date, yearRange, countryName);
+            let tempDiff = +calcTempDiff(currentTemp, previousYearTemp);
+
+
+            let finalTD;
+
+            let redRising = `<td style="padding:0 70px 0 15px;; color:#bf212f">&#x2191;  ${tempDiff}</td>`;
+            let greenEqual = `<td style="padding:0 70px 0 15px;; color:#006f3c">&#x3d;  ${tempDiff}</td>`;
+            let blueDecreasing = `<td style="padding:0 70px 0 15px;; color:#264b96">&#x2193;  ${tempDiff}</td>`;
+
+            switch (Math.sign(tempDiff)) {
+                case 1:
+                    finalTD = redRising;
+                    break;
+                case 0:
+                    finalTD = greenEqual;
+                    break;
+                case -1:
+                    finalTD = blueDecreasing;
+                    break;
+                default:
+                    finalTD = '';
+            }
+
+
+            let row =
+                `<tr>
+                <td style="padding:0 60px 0 20px;">${previousYear}</td>
+                <td style="padding:0 70px 0 20px;">${previousYearTemp}</td>
+                ${finalTD}
+            </tr>`;
+
+            return row;
+        };
+
+        let generateHTMLTemplate = (date, countryName, currentTemp, yearRange) => {
+
+            let HTMLString = `
+        <h2>${countryName} ${currentTemp === 'NaN' || currentTemp === null ? 'No Data' : currentTemp+'\u00B0C'}</h2>
+        <h3>${date}    </h3>
+        
+        <table style="">
+            <tr>
+                <th>Last 25 Years</th>
+                <th>Temperature(\u00B0C)</th>
+                <th>Difference(\u00B0C)</th>
+            </tr>
+            ${generateRowTemplate(date, countryName, currentTemp, (1 * yearRange))}
+            ${generateRowTemplate(date, countryName, currentTemp, (2 * yearRange))}
+            ${generateRowTemplate(date, countryName, currentTemp, (3 * yearRange))}
+            ${generateRowTemplate(date, countryName, currentTemp, (4 * yearRange))}
+            ${generateRowTemplate(date, countryName, currentTemp, (5 * yearRange))}
+        </table>`;
+
+            return HTMLString;
+        };
+
         countries.features.forEach((e, i) => {
             e['t1_fromTempDSCountry'] = countryTemp(e.properties.name);
             e['t2_topoDSCountry'] = e.properties.name;
-                // console.log(e)
         });
 
 
@@ -70,27 +141,19 @@
             .attr('d', pathGenerator)
             .merge(mapDataJoin)
             .attr('fill', d => {
-                //console.log(d.t1_fromTempDSCountry + '_' + d.properties.name + '_' + date)
                 if (d.t1_fromTempDSCountry === null || d.t1_fromTempDSCountry === '' || d.t1_fromTempDSCountry === 'NaN') {
                     return 'black'
                 }
-                //colorScale.domain().forEach(e => console.log(`${e} ${d.properties.name}`))
-                //console.log(colorScale.domain()[0])
-                //console.log(colorScale(d.t1_fromTempDSCountry) + " " + d.t1_fromTempDSCountry + " " + d.properties.name)
-                // return colorScale(d.t1_fromTempDSCountry)
                 return colorScale(d.t1_fromTempDSCountry)
             })
             .on("mouseover", function(d, i) {
-                return tooltip.style("hidden", false).html('hello' + d.t1_fromTempDSCountry);
+                return tooltip.style("hidden", false).html('<h2>No Data</h2>');
             })
             .on("mousemove", function(d) {
                 tooltip.classed("hidden", false)
                     .style("top", (event.pageY) + "px")
                     .style("left", (event.pageX + 10) + "px")
-                    .html(`<h2>${d.properties.name} ${
-                    d.t1_fromTempDSCountry === 'NaN' || d.t1_fromTempDSCountry === null ? 'No Data' : d.t1_fromTempDSCountry
-                }</h2>dfdf </br>
-                fdf`);
+                    .html(generateHTMLTemplate(date, d.properties.name, d.t1_fromTempDSCountry, 5));
             })
             .on("mouseout", function(d, i) {
                 tooltip.classed("hidden", true);
